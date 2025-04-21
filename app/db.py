cfc02d42d2946c84e63bdb9bbf6fe444243d2906
shruti <shruti.2295.chd@gmail.com>
2025-04-21 19:51:01 +0100

    add

--- app/db.py
+++ app/db.py
@@ -8,6 +8,12 @@ def get_db_connection():
     returns:
         mysql.connector.connection.MySQLConnection: A connection object to interact with the database.
     """
+    print("INFO: Connecting to the database...")
+    print("INFO: DB_HOST:", os.getenv('DB_HOST'))
+    print("INFO: DB_PORT:", os.getenv('DB_PORT'))
+    print("INFO: DB_USER:", os.getenv('DB_USER'))
+    print("INFO: DB_PASSWORD:", os.getenv('DB_PASSWORD'))
+    print("INFO: DB_NAME:", os.getenv('DB_NAME'))
     return mysql.connector.connect(
         host=os.getenv('DB_HOST'),
         port=os.getenv('DB_PORT'),
